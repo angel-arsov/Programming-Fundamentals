2291b92d5bd0a8a6d56d7926d794f649d3d3d811
angel-arsov <angel.g.arsov@gmail.com>
2017-01-25 10:55:57 +0200

    Added MultiplyEvensByOdds

---
+++ MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyEvensByOdds
+{
+    class MultiplyEvensByOdds
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            int sumOfOdds = GetSumOfOdds(number);
+            int sumOfEvens = GetSumOfEvens(number);
+            int result = sumOfEvens * sumOfOdds;
+            Console.WriteLine(result);
+        }
+        public static int GetSumOfOdds(int number)
+        {
+            int sum = 0;
+            while (number > 0) 
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 != 0) 
+                {
+                    sum += lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+        public static int GetSumOfEvens(int number)
+        {
+            int sum = 0;
+            while (number > 0) 
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 == 0) 
+                {
+                    sum += lastDigit;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
