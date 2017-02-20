8d18f0e185c045ca94b0ccb7d00f1d1f76fad3c0
angel-arsov <angel.g.arsov@gmail.com>
2017-02-20 13:16:41 +0200

    Added EnduranceRally

---
+++ EnduranceRally/Driver.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace EnduranceRally
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public double Fuel()
+        {
+            var fuel = (int)Name[0];
+            return fuel;
+        }
+    }
+}
---
+++ EnduranceRally/EnduranceRally.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnduranceRally
+{ 
+
+    public class EnduranceRally
+    {
+        static void Main()
+        {
+            var driversNames = Console.ReadLine().Split(' ').ToList();
+            var zonesNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            var chekcpointIndexes = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var listOfDrivers = new List<Driver>();
+            foreach (var item in driversNames)
+            {
+                var currentDriver = new Driver { Name = item };
+                listOfDrivers.Add(currentDriver);
+            }
+
+            var resultDictionary = new Dictionary<Driver, string>();
+            foreach (var driver in listOfDrivers)
+            {
+                var currentFuel = driver.Fuel();
+                for (int i = 0; i < zonesNumbers.Count; i++) 
+                {
+                    if (chekcpointIndexes.Contains(i)) 
+                    {
+                        currentFuel += zonesNumbers[i];
+                    }
+                    else
+                    {
+                        currentFuel -= zonesNumbers[i];
+                    }
+                    if (currentFuel <= 0) 
+                    {
+                        resultDictionary[driver] = $"reached {i}";
+                        break;
+                    }
+                }
+                if (currentFuel > 0) 
+                {
+                    resultDictionary[driver] = $"fuel left {currentFuel:F2}";
+                }
+            }
+
+            foreach (var result in resultDictionary)
+            {
+                Console.WriteLine($"{result.Key.Name} - {result.Value}");
+            }
+        }
+    }
+}
